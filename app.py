710009ce2dec8509105eff99e78acf67e9b184f9
Ved Patil <vedpatil1345@gmail.com>
2024-11-17 21:06:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -457,8 +457,8 @@ header {
 .animation {
     position: absolute;
     top: 0;
-    left: 10%;
-    width: 80%;
+    left: 0;
+    width: 100%;
     height: 100%;
     display: flex;
     justify-content: space-between;
